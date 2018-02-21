6e585b48f240956771b49d3764e0831eedb2d32e
marvA101 <dev@a101.qa>
2018-02-21 09:42:41 +0300

    First Commit
    just the first one

---
+++ Assets/mainGame.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainGame : MonoBehaviour {
+
+	float seconds;
+	float minutes;
+	float hours;
+	float totalTime;
+	string displayTime;
+
+	public Text Timer;
+	bool buttonPressed;
+
+	public GameObject losePanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(buttonPressed == true){
+			seconds += Time.deltaTime;
+			if(seconds >= 60){
+				seconds = 0;
+				minutes += 1;
+
+				if(minutes >= 60){
+					hours += 1;
+				}
+			}
+			Timer.text = hours.ToString("00")+":"+minutes.ToString("00")+":"+seconds.ToString("00");
+			totalTime = hours+minutes+seconds;
+
+			if(totalTime > PlayerPrefs.GetFloat("highScore")){
+				PlayerPrefs.SetFloat("highScore",totalTime);
+
+				PlayerPrefs.SetString("highScoreText",hours.ToString("00")+":"+minutes.ToString("00")+":"+seconds.ToString("00"));
+			}
+		}
+	}
+
+	public void buttonPress(){
+		buttonPressed = true;
+	}
+	public void buttonRelease(){
+		buttonPressed = false;
+		losePanel.SetActive(true);
+	}
+
+	public void tryAgain(){
+		SceneManager.LoadScene("mainscreen");
+	}
+}
---
+++ Assets/mainscreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainscreen : MonoBehaviour {
+
+	public Text highScoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+		highScoreDisplay.text = PlayerPrefs.GetString("highScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void startGame(){
+		SceneManager.LoadScene("maingame");
+	}
+}
